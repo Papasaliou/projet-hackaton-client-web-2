c2e9a6e84d82cdae27c701a5248c323f36ff3e3d
Mamadou SENE <senemamadou1999@gmail.com>
2024-11-06 13:27:13 +0000

    upload audio et affichage du resumé

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
-import {Component, ElementRef, OnInit} from '@angular/core';
-import {RouterLink, RouterOutlet} from '@angular/router';
-import {InnovativememoryService} from "./services/innovativememory.service";
-import {Discussion} from "../utils/discussion";
-import {HttpClientModule} from "@angular/common/http";
-import {FormsModule} from "@angular/forms";
-import {ChangeDetection} from "@angular/cli/lib/config/workspace-schema";
-import {NgIf} from "@angular/common";
-import {DiscussionService} from "./services/discussion.service";
-import {parseJson} from "@angular/cli/src/utilities/json-file";
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { InnovativememoryService } from "./services/innovativememory.service";
+import { Discussion } from "../utils/discussion";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { NgIf } from "@angular/common";
+import { DiscussionService } from "./services/discussion.service";
+
 declare function handleMenuOpen(): any;
 declare function handleMenuClose(): any;
-// declare function activatebtnSubmit(): any;
 
 @Component({
   selector: 'app-root',
@@ -25,114 +23,102 @@ declare function handleMenuClose(): any;
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent implements OnInit{
-file!:File;
-fileName:string = "";
-fileUpload!:boolean
-  audiofileUpload!:boolean
-  question!:string;
-  result!:string;
-  discussion!:Discussion;
-  audioFile!:File;
-  audioFileInput!:ElementRef;
+export class AppComponent implements OnInit {
+  file!: File;
+  fileName: string = "";
+  fileUpload: boolean = false;
+  audiofileUpload: boolean = false;
+  question!: string;
+  result!: string;
+  discussion!: Discussion;
+  audioFile!: File;
+  audioFileInput!: ElementRef;
+  fileInput!: ElementRef;
 
-fileInput!: ElementRef;
+  showFileUploadButton: boolean = false;
+  showAudioUploadButton: boolean = false;
+
+  constructor(
+    private innovativememoryService: InnovativememoryService,
+    private discussionService: DiscussionService
+  ) {}
 
   ngOnInit() {
     handleMenuClose();
   }
 
-  constructor(private innovativememoryService:InnovativememoryService,private  discussionService:DiscussionService) {
-  }
   title = 'app-web';
   protected readonly handleMenuClose = handleMenuClose;
   protected readonly handleMenuOpen = handleMenuOpen;
 
-
-  onFileSelected(event: Event) { // Changé de MouseEvent à Event
-    const fileInput = event.target as HTMLInputElement; // Corrigé la typo "fileInpute"
+  onFileSelected(event: Event) {
+    const fileInput = event.target as HTMLInputElement;
     if (fileInput.files && fileInput.files.length > 0) {
       this.file = fileInput.files[0];
-      this.fileName = this.file.name; // Pas besoin de split() car file.name donne déjà le nom du fichier
+      this.fileName = this.file.name;
       this.fileUpload = true;
+      this.showFileUploadButton = true;
+      this.showAudioUploadButton = false;
+      this.audiofileUpload = false;
     }
   }
 
-  onAudioFileSelected(event: Event) { // Changé de MouseEvent à Event
-    const audiofileInput = event.target as HTMLInputElement; // Corrigé la typo "fileInpute"
+  onAudioFileSelected(event: Event) {
+    const audiofileInput = event.target as HTMLInputElement;
     if (audiofileInput.files && audiofileInput.files.length > 0) {
       this.audioFile = audiofileInput.files[0];
-      this.fileName = this.file.name; // Pas besoin de split() car file.name donne déjà le nom du fichier
+      this.fileName = this.audioFile.name;
       this.audiofileUpload = true;
+      this.showAudioUploadButton = true;
+      this.showFileUploadButton = false;
+      this.fileUpload = false;
     }
   }
 
   chatComponent() {
     this.discussionService.question = this.question;
-    console.log("==========>"+this.discussionService.question);
-    this.discussion=new Discussion(this.question,true)
+    this.discussion = new Discussion(this.question, true);
     this.discussionService.discussions.push(this.discussion);
-    console.log(this.discussionService.discussions.length);
 
     this.innovativememoryService.chat(this.question).subscribe({
-      next : value => {
-        this.result = value;
-        this.discussion=new Discussion(value,false)
+      next: value => {
+        this.discussion = new Discussion(value, false);
         this.discussionService.discussions.push(this.discussion);
-        console.log(this.discussionService.discussions.length);
-        console.log("============>"+value);
       },
-      error:err => {
-        console.log("Pas de reponse"+JSON.stringify(err));
+      error: err => {
+        console.log("Pas de reponse" + JSON.stringify(err));
       }
-    })
+    });
   }
-
 
   uploadAudioFile() {
-    if(this.audioFile){
+    if(this.audioFile) {
       this.innovativememoryService.uploadAudio(this.audioFile).subscribe({
-        next:value => {
-          this.result = value;
-          this.discussion=new Discussion(value,false)
+        next: value => {
+          this.discussion = new Discussion(value, false);
           this.discussionService.discussions.push(this.discussion);
-          console.log(this.discussionService.discussions.length);
-          console.log("============>"+value);
-          // console.log("Le document est bien uploder");
         },
-        error:err => {
-          console.log("erreur lors de l'upload du document"+JSON.stringify(err));
+        error: err => {
+          console.log("erreur lors de l'upload du document" + JSON.stringify(err));
         }
-      })
+      });
     }
   }
-
 
   uploadFile() {
-    if(this.file){
+    if(this.file) {
       this.innovativememoryService.upload(this.file).subscribe({
-        next:value => {
+        next: value => {
           this.result = value;
-          this.discussion=new Discussion(value,false)
+          this.discussion = new Discussion(value, false);
           this.discussionService.discussions.push(this.discussion);
           console.log(this.discussionService.discussions.length);
-          console.log("============>"+value);
-          //console.log("Le document est bien uploder");
-
+          console.log("============>" + value);
         },
-        error:err => {
-          console.log("erreur lors de l'upload du document"+err);
+        error: err => {
+          console.log("erreur lors de l'upload du document" + err);
         }
-      })
+      });
     }
-
   }
-
-  // activatebtnSubmit($event: Ke) {
-  //   const miInput = document.getElementById("icon-buttonSubmit") as HTMLInputElement;
-  //   miInput.addEventListener("even",function (e){
-  //     miInput.setAttribute("style","visibility:collapse");
-  //   });
-  // }
-  // protected readonly activatebtnSubmit = activatebtnSubmit;
 }
--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,3 +1,3 @@
 export  class  Config{
-static apiUrl: string = "http://10.155.12.140:8080/api/innovativeMemory/";
+static apiUrl: string = "http://localhost:8080/api/innovativeMemory/";
 }
